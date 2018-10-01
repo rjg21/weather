878a329a337b1515edf9aa4f002962ff776c8a25
Robin Goodall <rjg21@cam.ac.uk>
2018-10-01 10:33:25 +0100

    Add support for country code

--- api/weather.py
+++ api/weather.py
@@ -1,47 +1,50 @@
-# test main.py
+"""Fetch weather information
+
+Usage:
+  weather (-h | --help)
+  weather [--country=COUNTRY] <city>
+
+Options:
+  -h, --help          Show a brief usage summary.
+  --country=COUNTRY Restrict cities to an ISO 3166 country code.
+
+An OpenWeatherMap API key MUST be provided via the OPENWEATHERMAP_KEY environment variable.
+"""
 import os
 import sys
 import requests
-
-# display usage
-def usage():
-    print('''
-Fetch weather information
-
-Usage:
-    weather <location>
-
-An OpenWeatherMap API key MUST be provided via the OPENWEATHERMAP_KEY environment variable.
-''')
+from docopt import docopt
 
 # environment key containing OpenWeatherMap Key
 envkey = 'OPENWEATHERMAP_KEY'
 
-# check if key set in environment
-if envkey not in os.environ:
-    print('Error: missing', envkey, 'environment variable')
-    usage()
-    exit(1)
+if __name__ == '__main__':
+    args = docopt(__doc__)
 
-# get key
-apikey = os.environ[envkey]
+    # check if key set in environment
+    if envkey not in os.environ:
+        print('Error: missing', envkey, 'environment variable')
+        exit(1)
 
-# check enough arguments
-if len(sys.argv) < 2:
-    print('Error: missing location argument')
-    usage()
-    exit(2)
+    # get key
+    apikey = os.environ[envkey]
 
-# get location argument
-location = sys.argv[1]
+    # get city argument
+    city = args['<city>']
 
-try:
-    r = requests.get('http://api.openweathermap.org/data/2.5/weather?APPID=' + apikey + '&units=metric&q=' + location)
-    r.raise_for_status()
-except:
-    print('Error: failed to get response from openweathermap for', location, '['+str(r.status_code)+']')
-    exit(r.status_code)
+    # get country argument
+    country = args['--country']
+    
+    # append country to city (with comma) if set
+    if (country is not None) and (country != ''):
+        city += ','+country
 
-data = r.json()
+    try:
+        r = requests.get('http://api.openweathermap.org/data/2.5/weather?APPID=' + apikey + '&units=metric&q=' + city)
+        r.raise_for_status()
+        data = r.json()
+    except:
+        print('Error: failed to get response from openweathermap for', city, '['+str(r.status_code)+']')
+        exit(r.status_code)
 
-print('Temperature for {0}, {1}: {2}\u2103'.format(data['name'], data['sys']['country'], data['main']['temp']))
+    print('Temperature for {0}, {1}: {2}\u2103'.format(data['name'], data['sys']['country'], data['main']['temp']))
